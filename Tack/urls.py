e520d055bf79971e09b5a8f7d877aff6d832a7e0
Jonathan <jonathanlambert247@gmail.com>
2018-12-28 12:12:25 -0800

    added group model and urls

--- Tack/urls.py
+++ Tack/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 #Import home app views
 from home import views as home_views
 
 urlpatterns = [
+    path('groups/', include('groups.urls')),
     path('', home_views.index, name="index"),
     path('admin/', admin.site.urls),
 ]
--- groups/models.py
+++ groups/models.py
@@ -5,4 +5,5 @@ from datetime import datetime
 #Group model
 class Group(models.Model):
     name = models.CharField(max_length=500)
+    u_id = models.CharField(max_length=8, default=0)
     date = models.DateTimeField(default=datetime.now)
