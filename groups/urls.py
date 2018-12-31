bb12814d7d073d469dc6460b6b736a150d3f7ee8
Jonathan <jonathanlambert247@gmail.com>
2018-12-31 01:04:05 -0800

    can create groups

--- groups/urls.py
+++ groups/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
-    
+    path('id/<u_id>', views.group, name="group"),
+    path('create-group', views.create_group, name="create_group"),
 ]
--- groups/views.py
+++ groups/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .models import Group
+
+import random, string
+
+#Create a new group
+def create_group(request):
+    if request.method == "POST":
+        name = request.POST["name"]
+        u_id = generate_id()
+        Group.objects.create(name=name, u_id=u_id)
+        return redirect("index")
+
+#Display group
+def group(request, u_id):
+    group = Group.objects.get(u_id=u_id)
+    return render(request, "groups/group.html", {"group": group})
+
+#Generate unique group id
+def generate_id():
+    unique = False
+    random_id = 0
+
+    #Check if id already exists
+    while not unique:
+        random_id = ''.join([random.choice(string.ascii_letters +
+            string.digits) for n in range(8)])
+
+        if Group.objects.filter(u_id=random_id).exists():
+            unique = False
+        else:
+            unique = True
+
+    return random_id
